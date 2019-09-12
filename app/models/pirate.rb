d0d8a03459876cd5131bafda7e2013192a965a3b
Tracy Reuther <tracyreuther@gmail.com>
2019-09-12 00:01:17 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.delete_all
+  end
 end
